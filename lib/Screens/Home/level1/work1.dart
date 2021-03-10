5057f74705c129d61f3b7b690efc0b9ea1fc7941
rimzouari97 <salamlem_97>
2021-03-10 18:40:02 +0100

    TT

--- lib/Screens/Home/level1/work1.dart
+++ lib/Screens/Home/level1/work1.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:orthophoniste/Screens/Home/level1/TileModel.dart';
 import 'package:orthophoniste/Screens/Home/level1/data.dart';
+
+import '../constants.dart';
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
@@ -36,99 +38,159 @@ class _HomeState extends State<Home> {
     });
   }
 
+  static const duration = const Duration(seconds: 1);
 
+  int secondsPassed = 0;
+  bool isActive = false;
+
+  Timer timer;
+
+  void handleTick() {
+    if (isActive) {
+      setState(() {
+        secondsPassed = secondsPassed + 1;
+      });
+    }
+  }
   @override
   Widget build(BuildContext context) {
+    if (timer == null) {
+      timer = Timer.periodic(duration, (Timer t) {
+        handleTick();
+      });
+    }
+    int seconds = secondsPassed % 60;
+    int minutes = secondsPassed ~/ 60;
+    int hours = secondsPassed ~/ (60 * 60);
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 50),
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 40,
-              ),
-              points != 800 ? Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    "$points/800",
-                    style: TextStyle(
-                        fontSize: 20, fontWeight: FontWeight.w500),
-                  ),
-                  Text(
-                    "Points",
-                    textAlign: TextAlign.start,
-                    style: TextStyle(
-                        fontSize: 14, fontWeight: FontWeight.w300),
-                  ),
-                ],
-              ) : Container(),
-              SizedBox(
-                height: 20,
-              ),
-              points != 800 ? GridView(
-                shrinkWrap: true,
-                //physics: ClampingScrollPhysics(),
-                scrollDirection: Axis.vertical,
-                gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                    mainAxisSpacing: 0.0, maxCrossAxisExtent: 100.0),
-                children: List.generate(gridViewTiles.length, (index) {
-                  return Tile(
-                    imagePathUrl: gridViewTiles[index].getImageAssetPath(),
-                    tileIndex: index,
-                    parent: this,
-                  );
-                }),
-              ) : Container(
-                  child: Column(
-                    children: <Widget>[
-                      GestureDetector(
-                        onTap: (){
-                          setState(() {
-                            points = 0;
-                            reStart();
-                          });
-                        },
-                        child: Container(
-                          height: 50,
-                          width: 200,
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                            color: Colors.blue,
-                            borderRadius: BorderRadius.circular(24),
-                          ),
-                          child: Text("Replay", style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 17,
-                              fontWeight: FontWeight.w500
-                          ),),
-                        ),
-                      ),
-                      SizedBox(height: 20,),
-                      GestureDetector(
-                        onTap: (){
-                          // TODO
-                        },
-                        child: Container(
-                          height: 50,
-                          width: 200,
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                                color: Colors.blue,
-                                width: 2
+      body: GestureDetector(
+        onTap: (){
+          setState(() {
+            isActive = !isActive;
+          });
+        },
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 50),
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 40,
+                ),
+                points != 800 ? Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "$points/800",
+                      style: TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.w500),
+                    ),
+                    Text(
+                      "Points",
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                          fontSize: 14, fontWeight: FontWeight.w300),
+                    ),
+                  ],
+                ) : Container(),
+                SizedBox(
+                  height: 20,
+                ),
+                points != 800 ? GridView(
+                  shrinkWrap: true,
+                  //physics: ClampingScrollPhysics(),
+                  scrollDirection: Axis.vertical,
+                  gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                      mainAxisSpacing: 0.0, maxCrossAxisExtent: 100.0),
+                  children: List.generate(gridViewTiles.length, (index) {
+                    return Tile(
+                      imagePathUrl: gridViewTiles[index].getImageAssetPath(),
+                      tileIndex: index,
+                      parent: this,
+                    );
+                  }),
+                ) : Container(
+                    child: Column(
+                      children: <Widget>[
+                        GestureDetector(
+                          onTap: (){
+                            setState(() {
+                              points = 0;
+                              reStart();
+                            });
+                          },
+                          child: Container(
+                            height: 50,
+                            width: 200,
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(
+                              color: Colors.blue,
+                              borderRadius: BorderRadius.circular(24),
                             ),
-                            borderRadius: BorderRadius.circular(24),
+                            child: Text("Replay", style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 17,
+                                fontWeight: FontWeight.w500
+                            ),),
                           ),
-
                         ),
-                      ),
-                    ],
-                  )
-              )
-            ],
+                        SizedBox(height: 20,),
+                        GestureDetector(
+                          onTap: (){
+                            // TODO
+                          },
+                          child: Container(
+                            height: 50,
+                            width: 200,
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: Colors.blue,
+                                  width: 2
+                              ),
+                              borderRadius: BorderRadius.circular(24),
+                            ),
+
+                          ),
+                        ),
+                      ],
+                    )
+                ),
+            Row(
+
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                LabelText(
+                    label: 'HRS', value: hours.toString().padLeft(2, '0')),
+                LabelText(
+                    label: 'MIN',
+                    value: minutes.toString().padLeft(2, '0')),
+                LabelText(
+                    label: 'SEC',
+                    value: seconds.toString().padLeft(2, '0')),
+              ],
+            ),
+                SizedBox(height: 60),
+                /*Container(
+                  width: 200,
+                  height: 47,
+                  margin: EdgeInsets.only(top: 5),
+                  child: RaisedButton(
+                    color: Colors.pink[200],
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25)),
+                    child: Text(isActive ? 'STOP' : 'START'),
+                    onPressed: () {
+                      setState(() {
+                        isActive = !isActive;
+                      });
+                    },
+                  ),
+                )*/
+              ],
+            ),
+
           ),
         ),
       ),
@@ -220,6 +282,40 @@ class _TileState extends State<Tile> {
           color: Colors.white,
           child: Image.asset("assets/correct.png"),
         ),
+      ),
+    );
+  }
+}
+class LabelText extends StatelessWidget {
+  LabelText({this.label, this.value});
+
+  final String label;
+  final String value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 5),
+      padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25),
+        color: Colors.white,
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Text(
+            '$value',
+            style: TextStyle(
+                color: Colors.black, fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            '$label',
+            style: TextStyle(
+              color: Colors.white70,
+            ),
+          ),
+        ],
       ),
     );
   }
