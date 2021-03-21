2f2c2b2e91af4ac0f74553cae13851433afabbf6
oussamaMaaroufi <oussama.maaroufi@esprit.tn>
2021-03-21 04:08:02 +0100

    commit 04:07  21/03/2021

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:orthophoniste/Screens/Welcome/welcome_screen.dart';
 import 'package:orthophoniste/backend/backHome.dart';
 import 'package:orthophoniste/constants.dart';
 import 'package:orthophoniste/services/user_service.dart';
+import 'package:orthophoniste/shared_preferences.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'Screens/Home/home.dart';
 import 'Screens/Home/level1/work1.dart';
@@ -15,21 +17,52 @@ void setupLocator() {
 void main() {
   setupLocator();
   runApp(MyApp());
+
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  SharedPref pref = SharedPref();
+  SharedPreferences _prefs ;
+  bool bb = true;
+   sConnect() async{
+   // _prefs = await SharedPreferences.getInstance();
+   // print("tesssssssssssssssssssssssst");
+     bb = await pref.isConnect();
+    print(bb);
+    return bb;
+  }
   @override
   Widget build(BuildContext context) {
+
+
+    sConnect();
+    if(bb) {
+    return MaterialApp(
+       debugShowCheckedModeBanner: false,
+       title: 'Flutter Auth',
+      theme: ThemeData(
+      primaryColor: kPrimaryColor,
+      scaffoldBackgroundColor: Colors.white,
+     ),
+
+        home :HomeScreen(),
+   //   home: backHome(),
+     );
+
+    }else {
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Auth',
       theme: ThemeData(
-        primaryColor:  kPrimaryColor,
+        primaryColor: kPrimaryColor,
         scaffoldBackgroundColor: Colors.white,
       ),
-      home: WelcomeScreen(),
-      //home: backHome(),
+
+      home :WelcomeScreen(),
+    //  home: backHome(),
     );
+  }
   }
 }
