a56d0e9c19fa390bc8d11db5197ec31d121bec36
oussamaMaaroufi <oussama.maaroufi@esprit.tn>
2021-03-26 17:34:43 +0100

    commit 17:34  26/03/2021

--- lib/Screens/Home/widgets/bottom_nav_bar.dart
+++ lib/Screens/Home/widgets/bottom_nav_bar.dart
@@ -8,8 +8,50 @@ class BottomNavBar extends StatelessWidget {
     Key key,
   }) : super(key: key);
 
+
   @override
   Widget build(BuildContext context) {
+//this gonna give us total height and with of our device
+    return MyBottomNavBar();
+  }
+
+}
+
+class MyBottomNavBar extends StatefulWidget {
+  @override
+  _BottomNavBarState createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<MyBottomNavBar> {
+
+  bool _Active= false;
+  void _toggle() {
+    setState(() {
+      _Active2 = false;
+      _Active1 = false;
+      _Active = true;
+    });
+  }
+  bool _Active1 = false;
+  void _toggle1() {
+    setState(() {
+      _Active2 = false;
+      _Active = false;
+      _Active1 = true;
+    });
+  }
+  bool _Active2 = false;
+  void _toggle2() {
+    setState(() {
+      _Active = false;
+      _Active1 = false;
+      _Active2 = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const bool active = false;
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
       height: 80,
@@ -20,21 +62,35 @@ class BottomNavBar extends StatelessWidget {
           BottomNavItem(
             title: "Today",
             svgScr: "assets/icons/calendar.svg",
+            isActive: _Active,
+            press: (){
+              _toggle();
+            },
           ),
           BottomNavItem(
             title: "All Exercises",
             svgScr: "assets/icons/gym.svg",
-            isActive: true,
+            isActive: _Active1,
+            press: (){
+              _toggle1();
+            },
           ),
           BottomNavItem(
             title: "Settings",
             svgScr: "assets/icons/Settings.svg",
+            isActive: _Active2,
+            press: (){
+              _toggle2();
+            },
           ),
         ],
       ),
     );
   }
+
 }
+
+
 
 class BottomNavItem extends StatelessWidget {
   final String svgScr;
@@ -69,3 +125,5 @@ class BottomNavItem extends StatelessWidget {
     );
   }
 }
+
+
