4144b6128bb05c436c94724182ba0ebbeffecbfd
rimzouari97 <salamlem_97>
2021-06-05 17:35:20 +0100

    17:35

--- lib/Screens/Home/level5/data/data.dart
+++ lib/Screens/Home/level5/data/data.dart
@@ -8,13 +8,13 @@ List<QuestionModel> questions = [
       id: 0,
       imageUrl:
           'https://images.unsplash.com/photo-1612099372193-4533390fa73f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
-      question: 'chat'),
+      question: 'kitten'),
   QuestionModel(
       unlock: false,
       id: 1,
       imageUrl:
           'https://images.unsplash.com/photo-1552519507-da3b142c6e3d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2Fyc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-      question: 'voiture'),
+      question: 'vehicule'),
   QuestionModel(
       unlock: false,
       id: 2,
